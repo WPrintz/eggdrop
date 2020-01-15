5713bdfd72f9f178453c559205eb56f90282ae7b
Wallace Printz <printw@amazon.com>
2020-01-15 00:17:17 -0600

    playing with cookies

--- web/main/start.py
+++ web/main/start.py
@@ -1,12 +1,26 @@
 # Reference: https://pythonhow.com/building-a-website-with-python-flask/
 
-from flask import Flask, render_template
+# from flask import Flask, render_template
+from flask import Flask, request, session, render_template, url_for, flash, redirect
+from forms import EntryForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4d612c2badbe0325280051d8ee4dc6'
 
-@app.route('/')
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = EntryForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            form_data = {"input": form}
+            session[data] = form_data
+            return redirect(url_for('go'))
+    elif request.method == 'GET':
+        return render_template('home.html', form=form)
 
 @app.route('/reset/')
 def reset():
